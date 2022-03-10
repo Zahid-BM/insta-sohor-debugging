d467c4eb9871d4502daa1846a54a02faddd89917
BM. Zahidul Islam <bmzahidulislam@outlook.com>
2022-03-11 00:44:16 +0600

    regular commit

--- js/app.js
+++ js/app.js
@@ -69,7 +69,7 @@ const createPost = (post) => {
                     target="_blank"
                     class="post__avatar"
                   >
-                    <img src="${userImage}" alt="User Picture" />
+                    <img src="${userImage /* bug fixed here */}" alt="User Picture" />
                   </a>
                   <a href="#" class="post__user">phero</a>
                 </div>
